003afd30cc9cd585987025b313b67b06e142d1dd
abrande <anbr0622@colorado.edu>
2019-12-10 15:00:36 -0500

    uneeded files

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import getRbuCustomerReducer from './getRbuCustomerReducer';
 import signUpReducer from './signUpReducer';
 import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
   form: formReducer,
-  userInfo: signUpReducer,
-  rbuCustomer: getRbuCustomerReducer
+  userInfo: signUpReducer
 });
