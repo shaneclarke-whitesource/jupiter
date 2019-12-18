54cc57d2fe1c4535d06ba089e21848d2968a0b85
Timothy Farrell <tim@thecookiejar.me>
2019-12-18 13:02:07 -0600

    Add an OK button to the modal

--- app/components/SignUp/SubmissionModal.js
+++ app/components/SignUp/SubmissionModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Modal from '../helix/Modal';
+import Button from '../helix/buttons/Button';
 import { withTranslation } from 'react-i18next';
 import { formValueSelector } from 'redux-form';
 
@@ -41,15 +42,24 @@ export class SubmissionModal extends React.Component {
   };
 
   returnModal = () => {
+    const { t, hideModal } = this.props;
     const message = this.responseMessage();
     return (
-      <Modal isOpen>
+      <Modal isOpen onClose={hideModal}>
         <Modal.Header>
           <h1>{message.header}</h1>
         </Modal.Header>
         <Modal.Body>
           <p>{message.message}</p>
         </Modal.Body>
+        <Modal.Footer>
+          <Button
+            type="button"
+            classNames="hxPrimary"
+            onClick={hideModal}
+            label={t('common.status.ok')}
+          />
+        </Modal.Footer>
       </Modal>
     );
   };
@@ -84,6 +94,7 @@ SubmissionModal.propTypes = {
     name: PropTypes.string,
     code: PropTypes.number
   }),
+  hideModal: PropTypes.func,
   t: PropTypes.func.isRequired
 };
 
