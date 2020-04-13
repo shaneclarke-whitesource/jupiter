ff457efcb3fab583a46003d5357f04c3f958eee4
abrande <anbr0622@colorado.edu>
2020-04-13 16:15:12 -0400

    fixing tests

--- app/components/SignUp/UserInfo.test.js
+++ app/components/SignUp/UserInfo.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { t } from '../../../test/i18n/mocks';
 import { UserInfo } from './UserInfo';
+import debounce from 'lodash/debounce';
+jest.useFakeTimers();
+
+jest.mock('lodash/debounce', () => jest.fn((fn) => fn));
 
 describe('UserInfo', () => {
   let wrapper;
-  const checkIfExistsMock = jest.fn();
+  const mock = jest.fn();
+  const checkIfExistsMock = debounce(mock, 150);
   const defaultProps = {
     checkIfExists: checkIfExistsMock,
     firstName: '',
@@ -40,11 +45,13 @@ describe('UserInfo', () => {
   test('username must grab only first two characters of firstName and lastName', () => {
     global.Math.random = () => '314315654';
     wrapper.setProps({ firstName: 'John', lastName: 'Doe' });
+    jest.runAllTimers();
     expect(wrapper.instance().props.checkIfExists).toBeCalledWith('jodo.4315');
   });
 
   test('setUsername to be called on props update', () => {
     wrapper.setProps({ firstName: 'John', lastName: 'Doe' });
+    jest.runAllTimers();
     expect(wrapper.instance().props.checkIfExists).toHaveBeenCalled();
   });
 });
